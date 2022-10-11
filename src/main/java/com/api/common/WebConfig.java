4f724afe2a354c60e120c7e8d804677870dc0666
yamadaT <yt.oraclexdan@gmail.com>
2022-10-11 20:46:53 +0900

    add : WebConfigに使用するパスワードエンコーダのBean設定
    WebConfigにパスワードエンコーダ(PasswordEncoder)のBean設定

--- src/main/java/com/api/common/WebConfig.java
+++ src/main/java/com/api/common/WebConfig.java
@@ -1,8 +1,19 @@
 package com.api.common;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebConfig {
-
+	
+	/**
+	 * 使用するパスワードエンコーダーを指定
+	 * @return {@link PasswordEncoder}
+	 */
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
